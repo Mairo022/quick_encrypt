741c2c4f2fa15158e2254264b210c4f3bd63d1cc
Mairo022 <Mairo022@outlook.com>
2024-12-17 23:13:54 +0200

    Simplify group commands

--- services/ConfigService.cs
+++ services/ConfigService.cs
@@ -141,86 +141,96 @@ public class ConfigService
 
     public GroupCommand CliFormatCommand(string input)
     {
-        var groupCommand = new GroupCommand();
+        if (input == "g --list") return new GroupCommand{ Action = GroupAction.List };
         
         var inputSplit = input.Split(" ");
-        var name = inputSplit.Length > 1 ? inputSplit[1] : "";
-        var actionOrFlag = inputSplit.Length > 2 ? inputSplit[2] : "";
-        var paths = inputSplit.Length > 3 ? inputSplit[3] : "";
-        var delete = inputSplit.Length > 4 ? inputSplit[4] : "";
-        
-        if (input == "g --list")
-        {
-            groupCommand.Action = GroupAction.List;
-            return groupCommand;
-        }
+        if (inputSplit.Length < 2) return new GroupCommand();
 
-        var groupProcess = AllowedArgumentsActions.unknown;
+        string[] properties = ["paths", "action", "delete"];
+        string[] flags = ["--delete", "--info"];
+        
+        var name = inputSplit[1];
+        
         var groupExists = Groups.TryGetValue(name, out var group);
+        var groupCommand = new GroupCommand();
 
-        if (!groupExists && inputSplit.Length is 2 or 3)
+        if (inputSplit.Length is 2 or 3 && !groupExists) return groupCommand;
+        if (inputSplit.Length is 2 && group == null) return groupCommand;
+        
+        if (groupExists && group != null)
         {
-            Console.WriteLine($"Group {name} does not exist");
-            return groupCommand;
+            groupCommand.Name = name;
+            groupCommand.Process = group.Action;
+            groupCommand.Paths.AddRange(group.Paths);
+            groupCommand.Delete = group.Delete;
         }
-        
-        groupCommand.Name = name;
-        
-        if (inputSplit.Length >= 3 && Enum.TryParse(actionOrFlag, true, out groupProcess));
         
         switch (inputSplit.Length)
         {
             case 2:
-                if (group == null) break;
-
                 groupCommand.Action = GroupAction.Execute;
-                groupCommand.Process = group.Action;
-                groupCommand.Paths.AddRange(group.Paths);
-                groupCommand.Delete = group.Delete;
-
                 break;
-            
             case 3:
-                var isFlag = actionOrFlag.StartsWith("--");
+                var isFlag = inputSplit[2].StartsWith("--");
 
                 if (isFlag)
                 {
-                    var flag = actionOrFlag;
+                    var flag = inputSplit[2];
+                    if (!flags.Contains(flag)) break;
                     
                     if (flag == "--delete") groupCommand.Action = GroupAction.Delete;
-                    else if (flag == "--info") groupCommand.Action = GroupAction.Info;
-                    else if (flag == "--list") groupCommand.Action = GroupAction.List;
-                    else Console.WriteLine($"Invalid flag: {flag}");
+                    if (flag == "--info") groupCommand.Action = GroupAction.Info;
 
                     break;
                 }
                 
-                if (AllowedArgumentsActions.unknown == groupProcess)
+                var isKvp = inputSplit[2].Contains('=');
+
+                if (isKvp)
                 {
-                    Console.WriteLine("Invalid action");
-                    break;
+                    var kvpSplit = inputSplit[2].Split("=");
+                    var property = kvpSplit[0];
+                    var value = kvpSplit[1];
+                    
+                    if (!properties.Contains(property)) break;
+
+                    if ("action" == property && Enum.TryParse(value, true, out AllowedArgumentsActions parsedKvpAction))
+                    {
+                        groupCommand.Action = GroupAction.Save;
+                        groupCommand.Process = parsedKvpAction;
+                    }
+                    
+                    if ("delete" == property && bool.TryParse(value, out var parsedDelete))
+                    {
+                        groupCommand.Action = GroupAction.Save;
+                        groupCommand.Delete = parsedDelete;
+                    }
+
+                    if ("paths" == property)
+                    {
+                        groupCommand.Action = GroupAction.Save;
+                        groupCommand.Paths.AddRange(value.Split(';'));
+                    }
                 }
-                
-                groupCommand.Action = GroupAction.Save;
-                groupCommand.Process = groupProcess;
-                
+
                 break;
-            
             case 4 or 5:
-                if (AllowedArgumentsActions.unknown == groupProcess)
-                {
-                    Console.WriteLine("Invalid action");
-                    break;
-                }
-                
-                groupCommand.Action = GroupAction.Save;
-                groupCommand.Process = groupProcess;
-                groupCommand.Paths.AddRange(paths.Split(';'));
-                groupCommand.Delete = bool.TryParse(delete, out var parsedDelete) && parsedDelete;
+                var action = inputSplit[2];
+                var paths = inputSplit[3];
+                var delete = inputSplit.ElementAtOrDefault(4) ?? bool.FalseString;
 
+                if (!Enum.TryParse(action, true, out AllowedArgumentsActions parsedAction)) break;
+
+                groupCommand.Action = GroupAction.Save;
+                groupCommand.Process = parsedAction;
+                groupCommand.Name = name;
+                groupCommand.Paths.AddRange(paths.Split(';'));
+                groupCommand.Delete = bool.TryParse(delete, out var parsedDelete2) && parsedDelete2;
+                
                 break;
         }
-        
+
+
         return groupCommand;
     }
     
@@ -230,7 +240,7 @@ public class ConfigService
         Console.WriteLine("Available group commands are: ");
         Console.WriteLine("1. Execute a group: g [group name]");
         Console.WriteLine("2. Create a group: g [group name] [action] [paths] [delete]");
-        Console.WriteLine("3. Modify a group: g [group name] [action] [paths](optional)");
+        Console.WriteLine("3. Modify a group: g [group name] [property]=value");
         Console.WriteLine("4. Delete a group: g [group name] --delete");
         Console.WriteLine("5. View group info: g [group name] --info");
         Console.WriteLine("6. View all groups: g --list");
