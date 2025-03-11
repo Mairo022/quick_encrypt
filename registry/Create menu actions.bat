df29d7de9a5a41b0516c2c960eb7a71a6501047b
Mairo022 <Mairo022@outlook.com>
2025-03-11 20:47:45 +0200

    Shorten context menu names

--- registry/Create menu actions.bat
+++ registry/Create menu actions.bat
@@ -9,32 +9,32 @@ set "regFile=%temp%\qe_to_reg.reg"
 echo Windows Registry Editor Version 5.00
 
 echo [HKEY_CLASSES_ROOT\*\shell\Quick Encrypt]
-echo @="Quick Encrypt"
+echo @="QE encrypt"
 echo [HKEY_CLASSES_ROOT\*\shell\Quick Encrypt\command]
 echo @="\"%programPath%\" path:\"%%1\" action:encrypt"
 
 echo [HKEY_CLASSES_ROOT\*\shell\Quick Encrypt Decrypt]
-echo @="Quick Decrypt"
+echo @="QE decrypt"
 echo [HKEY_CLASSES_ROOT\*\shell\Quick Encrypt Decrypt\command]
 echo @="\"%programPath%\" path:\"%%1\" action:decrypt"
 
 echo [HKEY_CLASSES_ROOT\*\shell\Quick Encrypt Delete]
-echo @="Quick Encrypt Delete"
+echo @="QE delete"
 echo [HKEY_CLASSES_ROOT\*\shell\Quick Encrypt Delete\command]
 echo @="\"%programPath%\" path:\"%%1\" action:delete"
 
 echo [HKEY_CLASSES_ROOT\Directory\shell\Quick Encrypt]
-echo @="Quick Encrypt"
+echo @="QE encrypt"
 echo [HKEY_CLASSES_ROOT\Directory\shell\Quick Encrypt\command]
 echo @="\"%programPath%\" path:\"%%1\" action:encrypt"
 
 echo [HKEY_CLASSES_ROOT\Directory\shell\Quick Encrypt Decrypt]
-echo @="Quick Decrypt"
+echo @="QE decrypt"
 echo [HKEY_CLASSES_ROOT\Directory\shell\Quick Encrypt Decrypt\command]
 echo @="\"%programPath%\" path:\"%%1\" action:decrypt"
 
 echo [HKEY_CLASSES_ROOT\Directory\shell\Quick Encrypt Delete]
-echo @="Quick Encrypt Delete"
+echo @="QE delete"
 echo [HKEY_CLASSES_ROOT\Directory\shell\Quick Encrypt Delete\command]
 echo @="\"%programPath%\" path:\"%%1\" action:delete"
 ) > "%regFile%"
